4ada66b6f8e1a750e5a3403c6898dd40102303aa
Ibrahim <ihindawi@gmail.com>
2022-04-16 20:09:37 +0400

    non blocking debug session

--- brun.bat
+++ brun.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" build\main.exe
+start "DebugRun" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" build\main.exe
