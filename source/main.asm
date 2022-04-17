490a54d5d107290a6253e130369e54a9883513a6
__ibrahim__ <ibrahim.hindawi@robocomvr.com>
2022-04-17 09:29:23 +0400

    added hello world! and modified build scripts

--- source/main.asm
+++ source/main.asm
@@ -6,9 +6,17 @@
 ;                                                                                               -
 ;                                                                                               -
 ;------------------------------------------------------------------------------------------------
-                                                includelib      kernel32.lib
                                                 ExitProcess     proto
-;----------[data section]------------------------------------------------------------------------
+												GetStdHandle	proto
+												WriteFile		proto
+STD_OUTPUT_HANDLE								equ				-11
+;----------[const section]-----------------------------------------------------------------------
+.const
+;outputmessagelength							qword			sizeof outputmessage
+outputmessage									byte			'Hello, World!'
+												byte			0AH, 0DH
+outputmessagelength								equ		 		$ - outputmessage
+;----------[data section]-------------------------------------------------------------------------
 .data
 var                                             qword           0FFFFh
 
@@ -18,6 +26,7 @@ main                                            proc
                                                 ;-----[multiplication]---------------------------
                                                 ; 64-bit multiplication results in 128-bit result
                                                 ; therefore rax and rdx are used
+                                                ;------------------------------------------------
                                                 xor             rax, rax                        ; clear rax
                                                 xor             rcx, rcx                        ; clear rcx
                                                 
@@ -26,8 +35,33 @@ main                                            proc
                                                 
                                                 mul             rcx
                                                 mov             var, rax
+
+												xor				rax, rax
+												xor				rcx, rcx
+
+												mov				rax, 1911
+												mov				rcx, var
+
+												mul				rcx 
+												mov				var, rax
+                                                ;-----[data output]------------------------------
+                                                ; print something to the console using WriteFile
+                                                ; write to std out
+                                                ;------------------------------------------------
+												sub				rsp, 40							; WriteFile(5 parms) * 8 = 40 bytes
+												mov				rcx, STD_OUTPUT_HANDLE
+												call			GetStdHandle
+
+												mov				rcx, rax
+												lea				rdx, outputmessage
+												mov				r8, outputmessagelength
+												xor				r9, r9
+												mov				[rsp + 32], r9					; nth parm - 1 = (5 - 4) * 8 = 32 bytes
+												call			WriteFile
                                                 
+												xor				rcx, rcx						; set termination code 0 for clean exit
                                                 call            ExitProcess
                                                 ret             0
 main                                            endp
 end
+
