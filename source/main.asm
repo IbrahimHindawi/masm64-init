6e63fab2c7fc0989f1d253b451d646d1a1d5cd44
ibrahim <ihindawi@gmail.com>
2022-04-20 12:35:45 +0400

    Update main.asm

--- source/main.asm
+++ source/main.asm
@@ -7,18 +7,18 @@
 ;                                                                                               ;
 ;-----------------------------------------------------------------------------------------------;
                                                 ExitProcess     proto
-												GetStdHandle	proto
-												WriteFile		proto
-												Sleep			proto
-std_output_handle								equ				-11
+                                                GetStdHandle    proto
+                                                WriteFile       proto
+                                                Sleep           proto
+std_output_handle                               equ             -11
 ;----------[const section]----------------------------------------------------------------------;
 .const
-;outputmessagelength							qword			sizeof outputmessage
-outputmessage									byte			'hello, world!'
-												byte			0ah, 0dh
-												byte			'from masm64!'
-												byte			0ah, 0dh
-outputmessagelength								equ		 		$ - outputmessage
+;outputmessagelength                            qword           sizeof outputmessage
+outputmessage                                   byte            'hello, world!'
+                                                byte            0ah, 0dh
+                                                byte            'from masm64!'
+                                                byte            0ah, 0dh
+outputmessagelength                             equ             $ - outputmessage
 ;----------[data section]-----------------------------------------------------------------------;
 .data
 var                                             qword           0ffffh
@@ -39,35 +39,34 @@ main                                            proc
                                                 mul             rcx
                                                 mov             var, rax
 
-												xor				rax, rax
-												xor				rcx, rcx
+                                                xor             rax, rax
+                                                xor             rcx, rcx
 
-												mov				rax, 1911
-												mov				rcx, var
+                                                mov             rax, 1911
+                                                mov             rcx, var
 
-												mul				rcx 
-												mov				var, rax
+                                                mul             rcx 
+                                                mov             var, rax
                                                 ;-----[data output]-----------------------------;
                                                 ; print something to the console using writefile
                                                 ; write to std out
                                                 ;-----------------------------------------------;
-												sub				rsp, 40							; writefile(5 parms) * 8 = 40 bytes
-												mov				rcx, std_output_handle
-												call			GetStdHandle
+                                                sub             rsp, 40                         ; writefile(5 parms) * 8 = 40 bytes
+                                                mov             rcx, std_output_handle
+                                                call            GetStdHandle
 
-												mov				rcx, rax
-												lea				rdx, outputmessage
-												mov				r8, outputmessagelength
-												xor				r9, r9
-												mov				[rsp + 32], r9					; nth parm - 1 = (5 - 4) * 8 = 32 bytes
-												call			WriteFile
+                                                mov             rcx, rax
+                                                lea             rdx, outputmessage
+                                                mov             r8, outputmessagelength
+                                                xor             r9, r9
+                                                mov             [rsp + 32], r9                  ; nth parm - 1 = (5 - 4) * 8 = 32 bytes
+                                                call            WriteFile
 
-												mov				rcx, 5000d
-												call			Sleep
+                                                mov             rcx, 5000d
+                                                call            Sleep
                                                 
-												xor				rcx, rcx						; set termination code 0 for clean exit
+                                                xor             rcx, rcx                        ; set termination code 0 for clean exit
                                                 call            ExitProcess
                                                 ret             0
 main                                            endp
 end
-
