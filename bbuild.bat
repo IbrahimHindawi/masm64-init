3e3e5e3ffb7ee4ae162ee1ad763c8f94c963b979
Ibrahim <ihindawi@gmail.com>
2022-06-22 18:31:30 +0400

    removed logo from build cmds

--- bbuild.bat
+++ bbuild.bat
@@ -14,13 +14,13 @@ set ml_directory="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\
 rem Set this value to the location of link.exe under the VC directory
 set link_directory="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64
 
-%rc_directory%\rc.exe" ..\resource\resource.rc
+%rc_directory%\rc.exe" /nologo ..\resource\resource.rc
 move ..\resource\resource.res .\
 
-%ml_directory%\ml64.exe" /c /Cp /Cx /Fm /FR /W2 /Zd /Zf /Zi ..\source\%project_name%.asm > errors.txt
+%ml_directory%\ml64.exe" /c /Cp /Cx /Fm /FR /W2 /Zd /Zf /Zi /nologo ..\source\%project_name%.asm > errors.txt
 
 rem %link_directory%\link.exe" %project_name%.obj resource.res /debug:full /opt:ref /opt:noicf /largeaddressaware:no /def:..\source\%project_name%.def /entry:main /machine:x64 /map /out:%project_name%.exe /PDB:%project_name%.pdb /subsystem:windows,6.0 "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.19041.0\um\x64\kernel32.lib" "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.19041.0\um\x64\user32.lib"
-%link_directory%\link.exe" %project_name%.obj resource.res /debug:full /opt:ref /opt:noicf /largeaddressaware:no /def:..\source\%project_name%.def /entry:main /machine:x64 /map /out:%project_name%.exe /PDB:%project_name%.pdb /subsystem:console "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.19041.0\um\x64\kernel32.lib" "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.19041.0\um\x64\user32.lib"
+%link_directory%\link.exe" %project_name%.obj resource.res /debug:full /nologo /opt:ref /opt:noicf /largeaddressaware:no /def:..\source\%project_name%.def /entry:main /machine:x64 /map /out:%project_name%.exe /PDB:%project_name%.pdb /subsystem:console "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.19041.0\um\x64\kernel32.lib" "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.19041.0\um\x64\user32.lib"
 type errors.txt
 
 popd
