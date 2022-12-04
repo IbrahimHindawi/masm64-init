d5360162f8f5911222d93c9c54fb995ffe57b699
__ibrahim__ <ibrahim.hindawi@robocomvr.com>
2022-12-04 16:13:32 +0400

    added simd ops

--- source/chapters/math.asm
+++ source/chapters/math.asm
@@ -15,6 +15,10 @@
 align                                           16                                                                                  ; misalignment will cause access violation
 nums0                                           dword                                           1, 2, 3, 4                          ; 128 bit aligned 4 x i32 vector
 nums1                                           dword                                           1, 3, 5, 7                          ; 128 bit aligned 4 x i32 vector
+fnum0                                           real4                                           1.5, 2.4, 3.8, 3.141592             ; 128 bit aligned 4 x f32 vector
+fnum1                                           real4                                           3.9, 1.6, 2.5, 8.23                 ; 128 bit aligned 4 x f32 vector
+dnum0                                           real8                                           1.5, 3.1415926535897932             ; 128 bit aligned 2 x f64 vector
+dnum1                                           real8                                           1.5, 3.1415926535897932             ; 128 bit aligned 2 x f64 vector
 ;----------[code section]-----------------------------------------------------------------------------------------------------------;
 .code
 
@@ -40,6 +44,31 @@ math                                            proc
                                                 paddd                                           xmm0, xmmword ptr [nums1]           ; packed integer add dword
                                                 movdqa                                          xmm1, xmmword ptr [nums1]           ; move double quadword aligned
                                                 psubd                                           xmm0, xmm1                          ; packed integer subtract dword
+
+                                                movaps                                          xmm0, xmmword ptr [fnum0]           ; move, aligned, packed, single
+                                                movapd                                          xmm1, xmmword ptr [dnum0]           ; move, aligned, packed, double
+
+                                                ;---[scalar ops]--------------------------------------------------------------------;
+                                                movss                                           xmm0, fnum0
+                                                movss                                           xmm1, fnum1
+
+                                                addss                                           xmm0, xmm1
+                                                mulss                                           xmm0, xmm1
+                                                subss                                           xmm0, xmm1
+                                                divss                                           xmm0, xmm1
+                                                ;---[vector ops]--------------------------------------------------------------------;
+                                                ;   addps addpd
+                                                ;   subps subpd
+                                                ;   mulps mulpd
+                                                ;   divps divpd
+                                                ;-----------------------------------------------------------------------------------;
+                                                movaps                                          xmm0, xmmword ptr [fnum0]
+                                                movaps                                          xmm1, xmmword ptr [fnum1]
+                                                divps                                           xmm0, xmm1
+
+                                                movapd                                          xmm2, xmmword ptr [dnum0]
+                                                movapd                                          xmm3, xmmword ptr [dnum1]
+                                                divpd                                           xmm2, xmm3
 
                                                 xor                                             rax, rax                            ; return zero
                                                 ret                                                                                 ; return
