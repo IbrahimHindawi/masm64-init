a9f1f526bb066e5a7e12d7ff9eee8958a62021ed
IbrahimHindawi <ihindawi@gmail.com>
2024-11-30 09:56:45 +0400

    changed name or linker aguments

--- scripts/build.bat
+++ scripts/build.bat
@@ -9,8 +9,8 @@ rc.exe /nologo ..\resource\resource.rc
 move ..\resource\resource.res .\
 
 set mlargs=/Cp /Cx /Fm /FR /W2 /Zd /Zf /Zi /nologo
-set largs=resource.res /debug:full /nologo /opt:ref /opt:noicf /largeaddressaware:no /def:..\source\%project_name%.def /entry:main /machine:x64 /map /out:%project_name%.exe /PDB:%project_name%.pdb /subsystem:console kernel32.lib user32.lib
+set linkargs=resource.res /debug:full /nologo /opt:ref /opt:noicf /largeaddressaware:no /def:..\source\%project_name%.def /entry:main /machine:x64 /map /out:%project_name%.exe /PDB:%project_name%.pdb /subsystem:console kernel32.lib user32.lib
 
-ml64.exe %mlargs% ..\source\%project_name%.asm /link %largs%
+ml64.exe %mlargs% ..\source\%project_name%.asm /link %linkargs%
 
 popd
